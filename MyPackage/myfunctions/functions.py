b34a298b24e5a2bd0a19c74ae46181121249c911
juanmciller <juan.m.ciller@gmail.com>
2023-12-29 09:15:43 +0100

    aaa

---
+++ MyPackage/myfunctions/functions.py
@@ -0,0 +1,7 @@
+def sayHi(name):
+    """This is my first function.
+
+    Args:
+        name (string): User name.
+    """
+    print(f"Hello {name}")
---
+++ MyPackage/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="myfirstpackage",
+    version="1.0.0",
+    description="Es un test de creacion de paquete",
+    author="Juan MC",
+    author_email="juanmc@gmail.com",
+    packages=["funciones"],
+)
